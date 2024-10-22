5d1fef68cfa8b41117139cf4b2928dbfeacbc18c
Dmitriyev <evseev.dmsr@gmail.com>
2024-10-22 11:43:23 +0300

    Add testing db sql

---
+++ db/testing_user.sql
@@ -0,0 +1,20 @@
+INSERT into users_testing VALUES
+(
+    1,
+
+    'Ivan',
+    'Ivanov',
+    'ivan@gmail.com',
+
+    '{"fb.com", "vk.com"}',
+
+    'Moscow',
+    'Russia',
+    '+79999999999',          
+    'https://linkedin.com/aaa111',
+
+    'ivan_ivanov',
+    111222,
+    '{"friendship"}',
+    '{"c++", "github"}'
+);
