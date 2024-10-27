10900054fee970ff6a18a5c96bca1fcba7977242
BBigInt <128796687+BBigInt@users.noreply.github.com>
2024-10-27 19:33:33 +0300

    feature logging and reports
    Логирование в текстовые логи и информирование админов в tg

---
+++ tg_bot/src/logging/logging_report.py
@@ -0,0 +1,52 @@
+from ..data.admitted_people import notified_people
+from ..loader import bot
+from aiogram.types import BufferedInputFile
+import logging
+from pathlib import Path
+
+error_log: Path = Path('files', 'logs', 'error')
+info_log: Path = Path('files', 'logs', 'info')
+
+info_logger = logging.getLogger(str(info_log))
+info_logger.setLevel(logging.INFO)
+error_logger = logging.getLogger(str(error_log))
+error_logger.setLevel(logging.ERROR)
+
+# настройка обработчика и форматировщика для логеров
+handler_user_logger = logging.FileHandler(f"{str(info_log)}.log")
+handler_error_logger = logging.FileHandler(f"{str(error_log)}.log")
+formatter_logger = logging.Formatter("%(asctime)s %(message)s")
+
+# добавление форматировщика к обработчику
+handler_user_logger.setFormatter(formatter_logger)
+handler_error_logger.setFormatter(formatter_logger)
+
+# добавление обработчиков к логерам
+info_logger.addHandler(handler_user_logger)
+error_logger.addHandler(handler_error_logger)
+
+
+async def report(description: str,
+                 extent: str = 'info',
+                 exception: Exception | None = None,
+                 image: BufferedInputFile | None = None,
+                 add_informed: list[int] | int | None = None) -> None:
+    informed: list[int] = notified_people.copy()
+    if add_informed is not None:
+        if type(add_informed) is str:
+            informed.append(add_informed)
+        elif type(add_informed) is list:
+            informed += add_informed.copy()
+    for admin_id in informed:
+        if image is None:
+            await bot.send_message(chat_id=admin_id, text=description)
+        else:
+            await bot.send_photo(chat_id=admin_id, photo=image, caption=description)
+    informed.clear()
+    if extent == 'info':
+        try:
+            info_logger.info(description)
+        except Exception as e:
+            print(f'логирование в report свалилось с\n{e}')
+        return
+    error_logger.exception(description if exception is None else description + '\n' + str(exception))
