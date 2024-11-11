3e26d137e399277d9cda80c46227a417af0b0174
cheshire <sergalexlinn@gmail.com>
2024-11-11 16:33:02 +0300

    feature notifications

---
+++ notifications/src/config.py
@@ -0,0 +1,16 @@
+import os
+
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    email_serv_pass: SecretStr
+    email_serv_sender: SecretStr
+
+    model_config = SettingsConfigDict(env_file=os.environ.get('DOTENV', '.env'), env_file_encoding='utf8')
+
+
+# При импорте файла сразу создастся и провалидируется объект конфига,
+# который можно далее импортировать из разных мест
+settings = Settings()
