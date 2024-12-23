b0ae39d1f0284b285596137bc735564fbbc84fcf
und3v3l0p3d <prohibitme@ya.ru>
2024-12-23 16:49:40 +0100

    add linter

--- packages/event_emitter/src/event_emitter/__init__.py
+++ packages/event_emitter/src/event_emitter/__init__.py
@@ -2,4 +2,4 @@ from .pubsub_event_emitter import PubsubEventEmitter
 from .emitter_interface import IProtoEmitter
 from .emitter_factory import EmitterFactory
 
-__all__ = ["IProtoEmitter", "EmitterFactory"]
+__all__ = ["IProtoEmitter", "EmitterFactory", "PubsubEventEmitter"]
