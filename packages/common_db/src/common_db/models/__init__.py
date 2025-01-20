54d9ac95bac6d92951b1f59e63ef99c59e26dfc9
epitanov <eles@MacBook-Pro-Eles.local>
2025-01-20 11:08:31 +0300

    fix missing on common_db

--- packages/common_db/src/common_db/models/__init__.py
+++ packages/common_db/src/common_db/models/__init__.py
@@ -3,6 +3,7 @@ from .users import ORMUserProfile
 from .meetings import ORMMeeting, ORMMeetingResponse
 from .meeting_intents import ORMMeetingIntent
 from .matching import ORMMatchingResult
+from .forms import ORMForm
 
 __all__ = [
     "Base",
@@ -12,4 +13,5 @@ __all__ = [
     "ORMMeetingResponse",
     "ORMMeetingIntent",
     "ORMMatchingResult",
+    "ORMForm",
 ]
