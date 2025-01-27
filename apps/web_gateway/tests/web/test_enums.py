14081a2ad013aa5e8c3185e1adf51d1d27a55a69
IOBiro <st.tell@mail.ru>
2025-01-27 03:59:52 +0300

    Remove trash changes

--- apps/web_gateway/tests/web/test_enums.py
+++
@@ -1,9 +0,0 @@
-from conftest import authorized_client
-from utils import assert_response_status
-
-
-def test_get_enums(authorized_client):
-
-    response = authorized_client.get("/enums/EIntentType")
-    assert_response_status(response, expected_status_code=200, msg_prefix="Get enum info failed.")
-
