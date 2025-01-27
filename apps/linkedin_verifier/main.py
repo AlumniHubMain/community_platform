adf8fca4a1d03b5bde2a2017fc8457c9ca5d42fd
tinyInt8 <98814561+tinyInt8@users.noreply.github.com>
2025-01-27 23:47:02 +0300

    patch main pubsub

--- apps/linkedin_verifier/main.py
+++ apps/linkedin_verifier/main.py
@@ -1,47 +1,38 @@
-from contextlib import asynccontextmanager
-from fastapi import FastAPI
 from loguru import logger
 import asyncio
-import os
 
-from src.api.v1.api import api_router
 from config import settings
-from src.linkedin.service import LinkedInService
 from src.pubsub.handlers import handle_profile_task
 from loader import broker
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
+async def main():
     try:
-        # Создаем LinkedIn сервис с готовым broker
-        # linkedin_service = LinkedInService(broker=broker)
-
-        # Запускаем подписку на задания
-        await broker.subscribe("my-topic-sub", handle_profile_task)
-
-        logger.info("Application startup complete")
-        yield
-
+        logger.info("Starting LinkedIn profile validator...")
+        
+        # Создаем задачу для подписки на PubSub
+        subscription_task = asyncio.create_task(
+            broker.subscribe(
+                settings.pubsub_linkedin_tasks_sub,
+                handle_profile_task
+            )
+        )
+        
+        logger.info("Started listening for LinkedIn profile tasks")
+        
+        # Держим приложение активным
+        while True:
+            await asyncio.sleep(1)
+            
     except Exception as e:
-        logger.error(f"Error during startup: {e}")
+        logger.error(f"Error in main loop: {e}")
         raise
+    finally:
+        logger.info("Shutting down...")
 
-
-app = FastAPI(
-    title=settings.PROJECT_NAME,
-    openapi_url=f"{settings.API_V1_STR}/openapi.json",
-    lifespan=lifespan
-)
-
-app.include_router(api_router, prefix=settings.API_V1_STR)
 
 if __name__ == "__main__":
-    import uvicorn
-
-    port = int(os.environ.get("PORT", 8000))  # Default to 8000 if not set
-    logger.info(f"Starting FastAPI application on port {port}")
-    uvicorn.run(app, host='0.0.0.0', port=port)
+    asyncio.run(main())
 
 
     # Example:
