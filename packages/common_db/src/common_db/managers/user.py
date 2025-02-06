3420ebbb34c8439d36ac3bcbbd6e3a34ab1b8ab8
cheshire <sergalexlinn@gmail.com>
2025-02-06 15:00:48 +0300

    style UserManager  add some base functions

--- packages/common_db/src/common_db/managers/user.py
+++ packages/common_db/src/common_db/managers/user.py
@@ -1,9 +1,21 @@
-from sqlalchemy import select, and_
+from fastapi import HTTPException, status
+from fastapi.responses import JSONResponse
+
+from sqlalchemy import select, and_, update
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
-from ..models.users import ORMUserProfile, ORMSpecialisation, ORMSkill
-from ..schemas.users import DTOUserProfileRead, DTOSearchUser
+from ..models.users import ORMUserProfile, ORMSpecialisation, ORMInterest, ORMSkill, ORMRequestsCommunity
+from ..schemas.users import (
+    DTOUserProfile,
+    DTOUserProfileUpdate,
+    DTOUserProfileRead,
+    DTOSearchUser,
+    DTOSpecialisation,
+    DTOInterest,
+    DTOSkill,
+    DTORequestsCommunity
+)
 
 
 class UserManager:
@@ -17,6 +29,18 @@ class UserManager:
             session: AsyncSession,
             user_id: int
     ) -> DTOUserProfileRead:
+        """
+        Get user by id.
+
+        Args:
+            session: database session
+            user_id: user identifier
+
+        Returns:
+            DTOUserProfileRead: user data
+        Raise:
+            HTTPException 404 if not found
+        """
         query = (
             select(ORMUserProfile)
             .options(
@@ -32,7 +56,260 @@ class UserManager:
 
         result = await session.execute(query)
         user = result.scalar_one_or_none()
-        return DTOUserProfileRead.model_validate(user) if user else None
+        if not user:
+            raise HTTPException(status_code=404, detail="Not found")
+        return DTOUserProfileRead.model_validate(user)
+
+    @classmethod
+    async def create_user(
+            cls,
+            session: AsyncSession,
+            user_data: DTOUserProfile
+    ) -> JSONResponse:
+        """
+        Create a new user in the database.
+
+        Args:
+            session: database session
+            user_data: user data for creation
+
+        Returns:
+            JSONResponse: response with status and created user id
+        """
+        user = ORMUserProfile(**user_data.model_dump(exclude_unset=True, exclude_none=True))
+        session.add(user)
+        await session.flush()
+        await session.commit()
+        return JSONResponse(
+            content={
+                "status": "success",
+                "message": "Create successfully",
+                "user_id": user.id
+            },
+            status_code=status.HTTP_201_CREATED
+        )
+
+    @classmethod
+    async def update_user(
+            cls,
+            session: AsyncSession,
+            user_data: DTOUserProfileUpdate
+    ) -> JSONResponse:
+        """
+        Update existing user in the database.
+
+        Args:
+            session: database session
+            user_data: user data for update
+
+        Returns:
+            JSONResponse: response with status and updated user id
+        """
+        user = await cls.get_user_by_id(session, user_data.id)
+
+        await session.execute(update(ORMUserProfile)
+                              .where(ORMUserProfile.id == user_data.id)
+                              .values(**user_data.model_dump(exclude_unset=True, exclude_none=True)))
+
+        await session.flush()
+        await session.commit()
+        return JSONResponse(
+            content={
+                "status": "success",
+                "message": "Updated successfully",
+                "user_id": user.id
+            },
+            status_code=status.HTTP_200_OK
+        )
+
+    @classmethod
+    async def get_user_by_tg_id(
+            cls,
+            session: AsyncSession,
+            user_tg_id: int
+    ) -> DTOUserProfileRead:
+        """
+        Get user by id.
+
+        Args:
+            session: database session
+            user_tg_id: user identifier
+
+        Returns:
+            DTOUserProfileRead: user data
+        Raise:
+            HTTPException 404 if not found
+        """
+        query = (
+            select(ORMUserProfile)
+            .options(
+                selectinload(ORMUserProfile.specialisations),
+                selectinload(ORMUserProfile.interests),
+                selectinload(ORMUserProfile.industries),
+                selectinload(ORMUserProfile.skills),
+                selectinload(ORMUserProfile.requests_to_community),
+                selectinload(ORMUserProfile.meeting_responses)
+            )
+            .where(ORMUserProfile.telegram_id == user_tg_id)
+        )
+
+        result = await session.execute(query)
+        user = result.scalar_one_or_none()
+        if not user:
+            raise HTTPException(status_code=404, detail="Not found")
+        return DTOUserProfileRead.model_validate(user)
+
+    @classmethod
+    async def get_all_specialisations(
+            cls,
+            session: AsyncSession
+    ) -> list[DTOSpecialisation]:
+        """
+        Get all specialisations from the database.
+
+        Args:
+            session: database session
+
+        Returns:
+            list[DTOSpecialisation]: list of all specialisations
+        """
+        query = select(ORMSpecialisation)
+        result = await session.execute(query)
+        specialisations = result.scalars().all()
+        return [DTOSpecialisation.model_validate(spec) for spec in specialisations]
+
+    @classmethod
+    async def get_all_specialisations_label(
+            cls,
+            session: AsyncSession
+    ) -> list[str]:
+        """
+        Get all labels of specialisations from the database.
+
+        Args:
+            session: database session
+
+        Returns:
+            list[DTOSpecialisation]: list of all labels of specialisations
+        """
+        query = select(ORMSpecialisation).filter(ORMSpecialisation.is_custom == False)
+        result = await session.execute(query)
+        specialisations = result.scalars().all()
+        return [DTOSpecialisation.model_validate(spec).label for spec in specialisations]
+
+    @classmethod
+    async def get_all_interests(
+            cls,
+            session: AsyncSession
+    ) -> list[DTOInterest]:
+        """
+        Get all interests from the database.
+
+        Args:
+            session: database session
+
+        Returns:
+            list[DTOInterest]: list of all interests
+        """
+        query = select(ORMInterest)
+        result = await session.execute(query)
+        interests = result.scalars().all()
+        return [DTOInterest.model_validate(interest) for interest in interests]
+
+    @classmethod
+    async def get_all_interests_label(
+            cls,
+            session: AsyncSession
+    ) -> list[str]:
+        """
+        Get all labels of interests from the database.
+
+        Args:
+            session: database session
+
+        Returns:
+            list[DTOInterest]: list of all labels of interests
+        """
+        query = select(ORMInterest).filter(ORMInterest.is_custom == False)
+        result = await session.execute(query)
+        interests = result.scalars().all()
+        return [DTOInterest.model_validate(interest).label for interest in interests]
+
+    @classmethod
+    async def get_all_skills(
+            cls,
+            session: AsyncSession
+    ) -> list[DTOSkill]:
+        """
+        Get all skills from the database.
+
+        Args:
+            session: database session
+
+        Returns:
+            list[DTOSkill]: list of all skills
+        """
+        query = select(ORMSkill)
+        result = await session.execute(query)
+        skills = result.scalars().all()
+        return [DTOSkill.model_validate(skill) for skill in skills]
+
+    @classmethod
+    async def get_all_skills_label(
+            cls,
+            session: AsyncSession
+    ) -> list[str]:
+        """
+        Get all labels of skills from the database.
+
+        Args:
+            session: database session
+
+        Returns:
+            list[str]: list of all labels of skills
+        """
+        query = select(ORMSkill).filter(ORMSkill.is_custom == False)
+        result = await session.execute(query)
+        skills = result.scalars().all()
+        return [DTOSkill.model_validate(skill).label for skill in skills]
+
+    @classmethod
+    async def get_all_requests_to_community(
+            cls,
+            session: AsyncSession
+    ) -> list[DTORequestsCommunity]:
+        """
+        Get all requests to community from the database.
+
+        Args:
+            session: database session
+
+        Returns:
+            list[DTORequestsCommunity]: list of all requests to community
+        """
+        query = select(ORMRequestsCommunity)
+        result = await session.execute(query)
+        requests = result.scalars().all()
+        return [DTORequestsCommunity.model_validate(request) for request in requests]
+
+    @classmethod
+    async def get_all_requests_to_community_label(
+            cls,
+            session: AsyncSession
+    ) -> list[str]:
+        """
+        Get all labels of requests to community from the database.
+
+        Args:
+            session: database session
+
+        Returns:
+            list[str]: list of all labels of requests to community
+        """
+        query = select(ORMRequestsCommunity).filter(ORMSkill.is_custom == False)
+        result = await session.execute(query)
+        requests = result.scalars().all()
+        return [DTORequestsCommunity.model_validate(request).label for request in requests]
 
     @classmethod
     async def search_users(
